707140b58bfbb4a68f04a0587c95b808e867ab18
Sudeep N <sudeep.n@urbanladder.com>
2016-04-21 14:00:10 +0530

    Printing decoded token and other details

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     token = request.headers['AuthToken']
-    rsa_public = OpenSSL::PKey::RSA.new File.read '../key.pub'
+    rsa_public = OpenSSL::PKey::RSA.new File.read '../key_final.pub'
     decoded_token = JWT.decode token, rsa_public, true, { :algorithm => 'RS256' } rescue -1
+    puts "\n\n\n\n\nToken: #{token}\n\nrsa_public: \n#{rsa_public}\n\ndecoded_token: #{decoded_token}\n\n"
     if decoded_token
       render :json => {
       :message => "All good. You only get this message if you're authenticated.",
